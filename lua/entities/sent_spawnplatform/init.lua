b9bda8e177951a855d67e13cc7fb19a5a628be5c
Lex Robinson <lexi@lexi.org.uk>
2012-11-11 16:12:18 +0000

    Looks like duplication is fixed

--- lua/entities/sent_spawnplatform/init.lua
+++ lua/entities/sent_spawnplatform/init.lua
@@ -6,9 +6,10 @@
 
 --[[ Make the platform dupable ]]--
 duplicator.RegisterEntityClass("sent_spawnplatform",  function(ply,  pos,  angles,  data)
+    PrintTable(data)
 			local ent = ents.Create("sent_spawnplatform");
 			ent:SetAngles(angles);
-			ent:SetPos(angles);
+			ent:SetPos(pos);
 			for key, value in pairs(data) do
 				if (key:sub(1, 2) == "k_") then
 					ent:SetKeyValue(key:sub(3), value);
@@ -18,7 +19,8 @@ duplicator.RegisterEntityClass("sent_spawnplatform",  function(ply,  pos,  angle
 			ent:Spawn();
 			ent:Activate();
 			ply:AddCount("sent_spawnplatform", ent);
-		end,  "Pos",  "Angles",  "Data");
+            return ent;
+		end,  "Pos",  "Angle",  "Data");
 AddCSLuaFile("shared.lua");
 include('shared.lua');
 local model1 = Model("models/props_c17/streetsign004e.mdl");
