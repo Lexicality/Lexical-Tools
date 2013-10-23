cfe21126413a6cd3f517b4fb645ff425c209e5f1
Lex Robinson <lexi@lexi.org.uk>
2013-10-23 21:10:48 +0100

    That didn't actually need a folder either

--- lua/entities/gmod_propcannon.lua
+++ lua/entities/gmod_propcannon.lua
@@ -1,9 +1,21 @@
 --[[
-	~ Prop Cannon (server) ~
+	~ Prop Cannon (shar'd) ~
 	~ Lexi ~
 --]]
-AddCSLuaFile( "shared.lua" )
-include('shared.lua')
+AddCSLuaFile();
+ENT.Type 			= "anim";
+if (WireLib) then
+	ENT.Base		= "base_wire_entity"
+else
+	ENT.Base		= "base_gmodentity"
+end
+ENT.PrintName		= "Prop Cannonv2";
+ENT.Author			= "Lexi"; --Duncan Stead
+ENT.Contact			= "lexi@lexi.org.uk"; --whohooo@yahoo.co.uk
+ENT.Spawnable		= false;
+ENT.AdminSpawnable	= false;
+
+if (CLIENT) then return end
 
 function ENT:Initialize()
 	self:PhysicsInit(SOLID_VPHYSICS);
