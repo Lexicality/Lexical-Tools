e17f20c2c5d66bc9c32a3731813ff410d7302a96
Lex Robinson <lexi@lexi.org.uk>
2013-10-23 21:12:56 +0100

    Fix command box

--- lua/weapons/gmod_tool/stools/commandbox.lua
+++ lua/weapons/gmod_tool/stools/commandbox.lua
@@ -45,8 +45,8 @@ function TOOL:UpdateGhost(ent, ply) --( ent, player )
 end
 
 function TOOL:Think()
-	if (SERVER and not SinglePlayer()) then return end
-	if (CLIENT and SinglePlayer()) then return end
+	if (SERVER and not game.SinglePlayer()) then return end
+	if (CLIENT and game.SinglePlayer()) then return end
 	local ent = self.GhostEntity;
 	local model = string.lower(self:GetClientInfo("model"));
 	if (not (IsValid(ent) and ent:GetModel() == model)) then
