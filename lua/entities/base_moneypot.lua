cfc0e1f5dc915cab8f7eface1d26b6459dccb4d3
Lex Robinson <lexi@lexi.org.uk>
2014-03-01 15:54:21 +0000

    Invalidate the money entity immediately to avoid race conditions
    Thanks to Dion Froese for pointing this out

--- lua/entities/base_moneypot.lua
+++ lua/entities/base_moneypot.lua
@@ -96,12 +96,12 @@ end
 
 function ENT:StartTouch(ent)
     if (self:IsMoneyEntity(ent) and not self:IsMoneyEntityInvalid(ent) and (ent.MoneyPotPause or 0) < CurTime()) then
+        self:InvalidateMoneyEntity(ent);
         ent.MoneyPotPause = CurTime() + 100 -- Fix a stupid glitch
         self:SetDTInt(0, self:GetDTInt(0) + ent.dt.amount);
         self:UpdateOverlay();
         self:UpdateWireOutputs(ent.dt.amount);
         ent:Remove();
-        self:InvalidateMoneyEntity(ent);
     end
 end
 
