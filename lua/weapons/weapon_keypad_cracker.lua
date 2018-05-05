97be6350a3fe1eeba788576e3ed65b672a18ea0b
Lex Robinson <lex@lex.me.uk>
2018-05-05 14:55:32 +0100

    ?? Unknown changes

--- lua/weapons/weapon_keypad_cracker.lua
+++ lua/weapons/weapon_keypad_cracker.lua
@@ -1,16 +1,19 @@
 --[[
-    Cracking base
-    Should serve for cracker, lockpick and super varients thereon
+    Keypad Cracker
+    Copyright (c) 2013 Lex Robinson
+    This code is freely available under the MIT License
 --]]
 
---SWEP.Category       = "DarkRP"
-SWEP.PrintName      = "Keypad Cracker v2"
+AddCSLuaFile();
+
+SWEP.Category       = "Roleplay"
+SWEP.PrintName      = "Keypad Cracker"
 SWEP.Slot           = 4
 SWEP.SlotPos        = 1
 SWEP.DrawAmmo       = false
 SWEP.DrawCrosshair  = true
 
-SWEP.Author         = "Lexi"
+SWEP.Author         = "Lex Robinson"
 SWEP.Instructions   = "Left click to crack a keypad";
 SWEP.Contact        = ""
 SWEP.Purpose        = ""
@@ -28,54 +31,21 @@ SWEP.Primary.DefaultClip    = -1
 SWEP.Primary.Automatic      = false
 SWEP.Primary.Ammo           = ""
 
-
 SWEP.Secondary.ClipSize     = -1
 SWEP.Secondary.DefaultClip  = -1
 SWEP.Secondary.Automatic    = false
 SWEP.Secondary.Ammo         = ""
 
---[[
-    START CUSTOMISATIONY BITS
-    OVERRIDE THESE IN YOUR DERIVATION
---]]
+function SWEP:GetPrintName()
+    return "Keypad Cracker";
+end
 
 SWEP.CrackTime = 15; -- seconds
 
 -- Utilities
 function SWEP:IsTargetEntity(ent)
-    return string.find(ent:GetClass(), "sent_keypad");
+    return ent:GetClass() == "sent_keypad";
 end
-function SWEP:DoNoise()
-    if (self.Owner:Team() ~= TEAM_HITMAN) then
-        self:EmitSound("buttons/blip2.wav", 60, 200)
-    end
-end
-function SWEP:GetPrintName()
-    return "A badly designed " .. self.ClassName .. "!";
-end
-
--- Required Callbacks
-function SWEP:OnSuccess()
-    if (CLIENT) then
-        return;
-    end
-    local ent = self.CrackTarget;
-    if (IsValid(ent)) then
-        ent:TriggerKeypad(true);
-    end
-end
-function SWEP:OnFailure()
-end
-
--- Misc callabcks
-function SWEP:OnStart()
-end
-function SWEP:OnReset()
-end
-
---[[
-    OK STOP OVERIDING SHIT NOW K
---]]
 
 local function cancrack(tr)
     return tr.HitNonWorld and tr.StartPos:Distance(tr.HitPos) <= 300
@@ -85,10 +55,44 @@ SWEP.Cracking = false;
 SWEP.CrackStart = 0;
 SWEP.CrackEnd = 0;
 SWEP.CrackTarget = NULL;
+SWEP.Timer = 0;
+
+function SWEP:ResetState()
+    self:SetWeaponHoldType("normal")
+    self.Cracking = false;
+    if (IsValid(self.CrackTarget)) then
+        self.CrackTarget.dt.Cracking = false;
+    end
+    self.CrackTarget = NULL;
+    self.Timer = 0;
+end
 
 function SWEP:Initialize()
     self:ResetState();
 end
+
+function SWEP:Succeed()
+    if (SERVER and IsValid(self.CrackTarget)) then
+        self.CrackTarget:TriggerKeypad(true);
+    end
+    self:ResetState();
+end
+
+function SWEP:Fail()
+    self:ResetState();
+end
+
+function SWEP:Holster()
+    self:Fail();
+    return true;
+end
+
+
+--[[
+
+        Meat & Veg
+
+--]]
 
 function SWEP:PrimaryAttack()
 	self:SetNextPrimaryFire(CurTime() + .4)
@@ -115,22 +119,13 @@ function SWEP:PrimaryAttack()
 
     self.Cracking = true;
     self.CrackTarget = ent;
-    self:OnStart();
+    ent.dt.Cracking = true;
     if (team == TEAM_ADMIN or team == TEAM_SADMIN) then
         self:Succeed();
         return;
     end
 end
 
-SWEP.Timer = 0;
-
-function SWEP:ResetState()
-    self:OnReset();
-	self:SetWeaponHoldType("normal")
-    self.Cracking = false;
-    self.CrackTarget = NULL;
-    self.Timer = 0;
-end
 
 local frames = {
     28, -- LED on
@@ -148,25 +143,11 @@ function SWEP:Think()
     if (not self.Cracking) then
         return;
     end
-    -- Make sure they haven't movedthrakerzod
+    -- Make sure they haven't moved
     local tr = self.Owner:GetEyeTrace();
     if (not (cancrack(tr) and tr.Entity == self.CrackTarget)) then
         self:Fail();
         return;
-    end
-    -- Timers
-    local ft = FrameTime();
-    self.Timer = self.Timer + FrameTime();
-    if (CLIENT) then
-        if (self.Timer > 0.5) then
-            self.Timer = 0;
-            self:_hudtimer();
-        end
-    else
-        if (self.Timer > 1) then
-            self.Timer = 0;
-            self:DoNoise();
-        end
     end
     -- Crack testing
     if (self.CrackEnd <= CurTime()) then
@@ -174,53 +155,10 @@ function SWEP:Think()
     end
 end
 
-function SWEP:Succeed()
-    self:OnSuccess();
-    self:ResetState();
-end
-
-function SWEP:Fail()
-    self:OnFailure();
-    self:ResetState();
-end
-
-function SWEP:Holster()
-    self:Fail();
-    return true;
-end
-
 SWEP.SecondaryAttack = SWEP.PrimaryAttack
 
 if (SERVER) then
     return;
-end
-
--- This code belongs to whoever wrote the lockpick SWEP. I disavow all ownership of it, including the edits I have made.
-local dots = {
-    [3] = "";
-    [0] = ".";
-    [1] = "..";
-    [2] = "...";
-}
-SWEP.Dots = "";
-function SWEP:_hudtimer()
-    self.Dots = dots[#self.Dots];
-end
-
-function SWEP:DrawHUD()
-    if (not self.Cracking) then
-        return;
-    end
-    local w = ScrW()
-    local h = ScrH()
-    local x,y,width,height = w/2-w/10, h/ 2, w/5, h/15
-    draw.RoundedBox(8, x, y, width, height, Color(10,10,10,120))
-
-    local status = (CurTime() - self.CrackStart) / self.CrackTime;
-    local BarWidth = status * (width - 16) + 8
-    draw.RoundedBox(8, x+8, y+8, BarWidth, height - 16, Color(255-(status*255), 0+(status*255), 0, 255))
-
-    draw.SimpleText("Cracking" .. self.Dots, "Trebuchet24", w/2, h/2 + height/2, Color(255,255,255,255), 1, 1)
 end
 
 local texes = {
@@ -261,4 +199,8 @@ function SWEP:ViewModelDrawn()
     cam.IgnoreZ(true);
     render.DrawSprite(spritepos, 1, 1, color_white);
     cam.IgnoreZ(false);
+end
+
+function SWEP:Think()
+    DebugInfo(1, self.Owner:GetViewModel():GetCycle());
 end
