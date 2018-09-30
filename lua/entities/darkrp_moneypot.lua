65f0aafbaab4bc8b5cf6e93e3306d2794174e9e3
Lex Robinson <lex@lex.me.uk>
2018-09-30 17:08:45 +0100

    Relicense project to Apache V2
    ALv2 is MIT but written by a lawyer, so it's better.

--- lua/entities/darkrp_moneypot.lua
+++ lua/entities/darkrp_moneypot.lua
@@ -1,7 +1,19 @@
 --[[
-    Dark-RP specific Moneypot
-    Copyright (c) 2010-2016 Lex Robinson
-    This code is freely available under the MIT License
+	Lexical Tools - lua/entities/darkrp_moneypot.lua
+    Copyright 2010-2016 Lex Robinson
+    This is a moneypot that only works with DarkRP money
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
 --]]
 
 AddCSLuaFile();
--- lua/weapons/gmod_tool/stools/fading_doors.lua
+++ lua/weapons/gmod_tool/stools/fading_doors.lua
@@ -1,7 +1,19 @@
 --[[
-	~ Fading Door STool ~
-	~ Based on Conna's, but this time it works. ~
-	~ Lexi ~
+	Lexical Tools - lua\weapons\gmod_tool\stools\fading_doors.lua
+	Copyright 2010-2011 Lex Robinson
+	Walk through props on demand, originally written by Conna
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
 --]]
 
 --[[ Tool Related Settings ]]--
