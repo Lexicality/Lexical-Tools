5df7e5ca7d454f63008e3c7ce2e4c8c1418a908f
Lex Robinson <lex@lex.me.uk>
2018-12-31 12:54:36 +0000

    Fix sanity check always being on
    Technically someone could have read the code and set their own config
    value to catch this, which will upset them - however this is a major
    version bump so :^)

--- lua/entities/sent_spawnplatform/sv_spawning.lua
+++ lua/entities/sent_spawnplatform/sv_spawning.lua
@@ -204,10 +204,9 @@ function ENT:GetSpawnClass()
 	end
 
 	local npcdata = list.Get('NPC')[class];
-	if (not npcdata and not npcspawner.config.allowdodgy) then
-		ErrorNoHalt(string.format("%s just tried to spawn NPC %s which is not on the NPC list!", self, class))
+	if (not npcdata and npcspawner.config.sanity) then
 		self:TurnOff();
-		return nil, nil;
+		error(string.format("%s just tried to spawn NPC %q which is not on the NPC list!", self, class))
 	end
 
 	return class, npcdata;
